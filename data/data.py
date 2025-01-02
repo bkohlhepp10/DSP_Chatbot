bc1007c9341e8f876ac63314a57d81d91cb55f85
Bennett Kohlhepp <bennettkohlhepp@gmail.com>
2025-01-02 16:35:59 -0600

    Datasets for data analysis

---
+++ data/data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# This dataset has information about symptoms of various diseases
+symptom_data = pd.read_csv('data/dataset.csv')
+
+# This dataset has information about precautions for various diseases
+precaution_data = pd.read_csv('data/precaution.csv')
+
+print(symptom_data.head())
+print(precaution_data.head())
+
+
+
